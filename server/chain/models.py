44adf5d7d11ebce7e4d2acaa98c1bad1471ddcac
Adarsh S <4d4r5h.5@gmail.com>
2017-07-01 20:55:51 +0530

    Define models for Chain app

---
+++ server/chain/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Chain(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=70, blank=True)
+    address = models.CharField(max_length=300)
+    contact_number = models.CharField(max_length=100)
+    number_of_stores = models.IntegerField(default=1)
+
+    def __unicode__(self):
+        return self.name
