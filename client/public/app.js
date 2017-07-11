995a6d27893379dcb8b23b776a7ee1857af429b4
jjmachan <jamesjithin97@gmail.com>
2017-07-11 15:58:40 +0530

    added home view

--- client/public/app.js
+++ client/public/app.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var signup = angular.module("signup", []);
+var home = angular.module("home", []);
 
 angular
     .module('SampleApplication', [
         'appRoutes',
-        'signup'
+        'signup',
+        'home'
     ]);
--- client/public/appRoutes.js
+++ client/public/appRoutes.js
@@ -1,13 +1,21 @@
 angular
     .module('appRoutes', ["ui.router"])
-    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
 
     $stateProvider.state({
         name: 'signup',
-        url: '/',
+        url: '/signup',
         templateUrl: 'public/components/signup/signup.template.html',
         controller: 'SignupController'
     });
 
+    $stateProvider.state({
+        name: 'home',
+        url: '/',
+        templateUrl: 'public/components/home/home.template.html',
+        controller: 'HomeController'
+    });
+
     $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
 }]);
