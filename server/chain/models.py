be05ccc2d5aa133201b198a7918773eed2838327
Adarsh S <4d4r5h.5@gmail.com>
2017-07-12 21:10:10 +0530

    Set unique code for chains

--- server/chain/models.py
+++ server/chain/models.py
@@ -3,8 +3,8 @@ from django.core.validators import RegexValidator
 
 
 class Chain(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    chain_code = models.CharField(validators=[RegexValidator(regex='^.{6}$', message='Length has to be 4', code='nomatch')])
+    name = models.CharField(max_length=100)
+    chain_code = models.CharField(max_length=6, unique=True)
     email = models.EmailField(max_length=70, unique=True)
     address = models.CharField(max_length=300)
     contact_number = models.CharField(max_length=10)
