181764debab15874295f0942340c3b2bd0196c08
jjmachan <jamesjithin97@gmail.com>
2017-07-14 13:00:11 +0530

    fixed forms

--- client/public/components/businessSignup/businessSignup.controller.js
+++ client/public/components/businessSignup/businessSignup.controller.js
@@ -1,6 +1,17 @@
 businessSignup
 	.controller('BusinessSignupController',['$scope','$location','userService','flashService',
-		function($scope,$location,userService,flashService){
+		function($scope,$location,chainService,flashService){
 			$scope.message = 'hello';
 		}
-		]);
+		])
+	.factory('chainService', function($resource) {
+        return $resource(
+            'https://jsonplaceholder.typicode.com/users',
+            {},
+            {
+            	'save': {
+                    method: 'POST'
+                	}
+
+            });
+    });
