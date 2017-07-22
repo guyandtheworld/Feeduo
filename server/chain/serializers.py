a1412c10264fa8ade954a73f1acd91445639bf6e
Adarsh S <4d4r5h.5@gmail.com>
2017-07-22 18:47:36 +0530

    Add views and serializers

--- server/chain/serializers.py
+++ server/chain/serializers.py
@@ -3,8 +3,45 @@ from rest_framework import serializers
 from models import Chain
 
 
-class ChainSerializer(serializers.ModelSerializer):
+class ChainSerializer(serializers.Serializer):
     class Meta:
         model = Chain
         fields = ('name', 'chain_code', 'email',)
 
+class ChainRegistrationSerializer(serializers.ModelSerializer):
+    chain_code = serializers.CharField()
+    name = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=30)
+    comfirm_password = serializers.CharField(max_length=30)
+    address = serializers.CharField()
+    contact_number = serializers.CharField(max_length=10)
+    number_of_stores = serializers.IntegerField()
+
+    class Meta:
+        model = Chain
+        field = "__all__"
+
+    def validate_email(self, email):
+        existing = Chain.objects.filter(email=email).first()
+        if existing:
+            raise serializers.ValidationError("A Chain with that email "
+                "address has already registered. Was it you?")
+        return email
+
+    def validate_chain_code(self, chain_code):
+        existing = Chain.objects.filter(chain_code=chain_code).first()
+        if existing:
+            raise serializers.ValidationError("A Chain with that chain_code "
+                "has been already registered. Was it you?")
+        return chain_code
+
+    def validate(self, data):
+        if not data.get('password') or not data.get('confirm_password'):
+            raise serializers.ValidationError("Please enter a password and "
+                "confirm it.")
+
+        if data.get('password') != data.get('confirm_password'):
+            raise serializers.ValidationError("Those passwords don't match.")
+
+        return data
--- server/chain/views.py
+++ server/chain/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from models import Chain
-from serializers import ChainSerializer
+from serializers import ChainSerializer, ChainRegistrationSerializer
 
 from permissions import IsGetOrIsAuthenticated
 
@@ -24,12 +24,10 @@ class ChainList(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        serializer = ChainSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = ChainRegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 
 class ChainDetail(APIView):
